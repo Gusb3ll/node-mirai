e9a6d02b70d91c09fd14b375c03518fa2000af9c
_StarChaser <32775520+ST4RCHASER@users.noreply.github.com>
2023-08-30 14:31:34 +0700

    fix: move to native fetch

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,14 @@
-import { upload, readFile } from "../src";
+import { upload } from "../src";
 //
-(async () => { 
-  const img = await readFile('example/test.png')
-  const imgUrl = await upload(img)
-  console.log('Image :', imgUrl)
+(async () => {
+  const img = await upload('./example/test.png', {
+    uploadType: '0'
+  })
+  console.log('Image :', img.url)
 
-  const video = await readFile('example/test.mp4')
-  const videoUrl = await upload(video)
-  console.log('Video :', videoUrl)
+  const video = await upload('./example/test.mp4')
+  console.log('Video :', video.url)
+
+  const text = await upload('example/test.txt')
+  console.log('Text :', text.url)
 })()
